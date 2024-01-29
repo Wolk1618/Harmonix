a663a1c6549ddda7a28ffc60c26bb9ff818c165b
Thomas <thomas.koc92@gmail.com>
2024-01-29 15:25:43 +0000

    add IMUs code on Arduino side

--- arduino_code/LEONARDO_UART/LEONARDO_UART.ino
+++ arduino_code/LEONARDO_UART/LEONARDO_UART.ino
@@ -1,7 +1,11 @@
 #include <SoftwareSerial.h>
+#include <Adafruit_LSM6DS3TRC.h>
 
 #define SIZE 64
+#define IMU1_I2C_ADDRESS 0x6A
+#define IMU2_I2C_ADDRESS 0x6B
 
+Adafruit_LSM6DS3TRC imu1, imu2;
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 int values1[SIZE]; 
@@ -9,9 +13,24 @@ int values2[SIZE];
 int index1 = 0;
 int index2 = 0;
 
+
 void setup() {
   Serial.begin(115200);
   mySerial.begin(115200);
+
+  Serial.println("Adafruit LSM6DS3TR-C tests");
+  
+  if (!imu1.begin_I2C(IMU1_I2C_ADDRESS)) {
+    Serial.println("Failed to initialize IMU 1!");
+    while (1);
+  }
+
+  if (!imu2.begin_I2C(IMU2_I2C_ADDRESS)) {
+    Serial.println("Failed to initialize IMU 2!");
+    while (1);
+  }
+
+  Serial.println("Both IMUs working");
 }
 
 void loop() {
@@ -19,6 +38,40 @@ void loop() {
     String receivedChunk = mySerial.readStringUntil(';');
     processChunk(receivedChunk);
   }
+
+  readIMUData(imu1, "IMU 1");
+  readIMUData(imu2, "IMU 2");
+
+  delay(100); // Adjust the delay if needed
+}
+
+void readIMUData(Adafruit_LSM6DS3TRC& imu, const char* name) {
+  sensors_event_t accel, gyro, temp;
+  imu.getEvent(&accel, &gyro, &temp);
+  
+  Serial.print(name);
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
 }
 
 void processChunk(String chunk) {
