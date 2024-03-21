17fd11662a56232b4657e677da0f1b5e1717adf0
Thomas <thomas.koc92@gmail.com>
2024-03-21 00:39:57 +0000

    cleaning up repo

--- useful_scripts/setup_midi.py
+++ useful_scripts/setup_midi.py
@@ -3,4 +3,4 @@ import mido
 print("Available MIDI ports:")
 available_ports = mido.get_output_names()
 for i, port in enumerate(available_ports):
-    print(f"{i}: {port}\n")
+    print(f"{i}: {port}\n")
